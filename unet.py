e2ac9bea61384d749e5463f463ea955a4ce550d8
Verena Kaynig-Fittkau <vkaynig@seas.harvard.edu>
2016-06-08 16:49:44 -0400

    I hate merge errors

--- unet.py
+++ unet.py
@@ -28,12 +28,14 @@ doTrain = int(sys.argv[1])
 patchSize = 572 #140
 patchSize_out = 388 #132
 
-weight_decay = 1.0
+weight_decay = 0.0
+weight_class_1 = 2.
+
 patience = 2
 
 purpose = 'test'
 initialization = 'glorot_uniform'
-filename = 'unet_weight_decay_1'
+filename = 'unet_weighted_class_2'
 
 # need to define a custom loss, because all pre-implementations
 # seem to assume that scores over patch add up to one which
@@ -41,7 +43,7 @@ filename = 'unet_weight_decay_1'
 def unet_crossentropy_loss(y_true, y_pred):
     epsilon = 1.0e-4
     y_pred_clipped = T.clip(y_pred, epsilon, 1.0-epsilon)
-    loss_vector = -T.mean(y_true * T.log(y_pred_clipped) + (1-y_true) * T.log(1-y_pred_clipped), axis=1)
+    loss_vector = -T.mean(weight_class_1*y_true * T.log(y_pred_clipped) + (1-y_true) * T.log(1-y_pred_clipped), axis=1)
     average_loss = T.mean(loss_vector)
     return average_loss
 
